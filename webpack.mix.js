a152fd0599fb2a943ee5d71f3a38002290d05361
Александр sandro <kipcode@gmail.com>
2020-09-09 15:03:02 +0300

    laravel mix setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.copyDirectory('resources/backend', 'public/backend');
+mix.copyDirectory('resources/frontend', 'public/frontend');
+
+/*
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+*/
