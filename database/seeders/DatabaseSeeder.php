5a6c8b5fc77cdd7390e2915eccbde03a63e9a887
Александр sandro <kipcode@gmail.com>
2020-09-09 19:50:03 +0300

    attributes 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(AdminsTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(AttributesTableSeeder::class);
     }
 }
