627abd2906f0a3b3179637af4b4755ccd1184568
Александр sandro <kipcode@gmail.com>
2020-09-09 19:23:18 +0300

    Categories

--- routes/admin.php
+++ routes/admin.php
@@ -12,8 +12,19 @@ Route::group(['prefix'  =>  'admin'], function () {
         })->name('admin.dashboard');
     });
   
+    /*settings*/
     Route::get('/settings', 'App\Http\Controllers\Admin\SettingController@index')->name('admin.settings');
     Route::post('/settings', 'App\Http\Controllers\Admin\SettingController@update')->name('admin.settings.update');
+  
+    /*categories*/
+    Route::group(['prefix'  =>   'categories'], function() {
+      Route::get('/', 'App\Http\Controllers\Admin\CategoryController@index')->name('admin.categories.index');
+      Route::get('/create', 'App\Http\Controllers\Admin\CategoryController@create')->name('admin.categories.create');
+      Route::post('/store', 'App\Http\Controllers\Admin\CategoryController@store')->name('admin.categories.store');
+      Route::get('/{id}/edit', 'App\Http\Controllers\Admin\CategoryController@edit')->name('admin.categories.edit');
+      Route::post('/update', 'App\Http\Controllers\Admin\CategoryController@update')->name('admin.categories.update');
+      Route::get('/{id}/delete', 'App\Http\Controllers\Admin\CategoryController@delete')->name('admin.categories.delete');
+    });
 
 });
 ?>
